336392898fd3eb72a2f6308f0927d37baa177763
caeamer <shangguanmoge@gmail.com>
2026-08-22 00:04:23 +0800

    chore: bump version to 1.3.0 (10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.lingoflow.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.2.0"
+        versionCode = 10
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
